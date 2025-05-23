8a1663a14ab293bb93fa17e24a0a57ad0cb2b4fd
Kamila <kamila.matuszak@list.pl>
2025-05-23 10:07:04 +0200

    Tube teleportation finalised

--- HamsterDevelopment/Assets/Scripts/Prototypes/Mechanics/DoEventOnTriggerCollision.cs
+++ HamsterDevelopment/Assets/Scripts/Prototypes/Mechanics/DoEventOnTriggerCollision.cs
@@ -7,15 +7,52 @@ public class DoEventOnTriggerCollision : MonoBehaviour
 {
     [SerializeField] private UnityEvent onCollisionEvents;
     [SerializeField] private List<string> collidingTags;
+    [SerializeField] private bool onButtonInteraction;
+    [SerializeField] private KeyCode buttonToUse;
     [Space(10)]
     [SerializeField] private bool isDebug;
+
+    private bool _isColliding;
+
+    private void Awake()
+    {
+        if (onButtonInteraction)
+        {
+            SuperDebug.Log($"{buttonToUse}");
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         foreach (var tag in collidingTags)
         {
-            if (other.gameObject.CompareTag(tag))
+            if (other.CompareTag(tag))
             {
-                if (isDebug) SuperDebug.Log("Colliding!");
+                _isColliding = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _isColliding = false;
+        SuperDebug.Log("Removing collider!");
+    }
+
+    private void Update()
+    {
+        if (_isColliding)
+        {
+            if (onButtonInteraction)
+            {
+                if (Input.GetKeyUp(buttonToUse))
+                {
+                    if (isDebug) SuperDebug.Log("Triggered!");
+                    onCollisionEvents?.Invoke();
+                }
+            }
+            else
+            {
                 onCollisionEvents?.Invoke();
             }
         }
