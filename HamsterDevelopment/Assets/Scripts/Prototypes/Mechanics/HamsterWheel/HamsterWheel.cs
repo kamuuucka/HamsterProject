aa7c33a7157ecc1ad8165ad7d389289670505edd
Kamila <kamila.matuszak@list.pl>
2025-08-12 13:42:17 +0200

    HamsterWheel.cs commented

--- HamsterDevelopment/Assets/Scripts/Prototypes/Mechanics/HamsterWheel/HamsterWheel.cs
+++ HamsterDevelopment/Assets/Scripts/Prototypes/Mechanics/HamsterWheel/HamsterWheel.cs
@@ -8,9 +8,13 @@ namespace Prototypes.Mechanics.HamsterWheel
     /// </summary>
     public class HamsterWheel : MonoBehaviour
     {
+        [Tooltip("The point where the hamster will be attached. The hamster will take both position and rotation from it.")]
         [SerializeField] private Transform attachPoint;
+        [Space(10)]
+        [SerializeField] private bool isDebug;
 
-        private Transform _lastKnownTransform;
+        private Vector3 _lastKnownPosition;
+        private Quaternion _lastKnownRotation;
 
         /// <summary>
         /// Attaches the object with the CharacterMovement script to the AttachPoint.
@@ -18,10 +22,33 @@ namespace Prototypes.Mechanics.HamsterWheel
         /// <param name="player">Object that has the Character Movement script.</param>
         public void AttachToWheel(CharacterMovement player)
         {
-            _lastKnownTransform = player.transform;
-            player.transform.position = attachPoint.position;
-            player.transform.localRotation = attachPoint.localRotation;
+            if (isDebug) SuperDebug.Log("Hamster attached!");
+
+            var playerTransform = player.transform;
+            
+            _lastKnownPosition = playerTransform.position;
+            _lastKnownRotation = playerTransform.rotation;
+            
+            playerTransform.position = attachPoint.position;
+            playerTransform.localRotation = attachPoint.localRotation;
+            
             player.GetCharacterController().enabled = false;
+        }
+
+        /// <summary>
+        /// Detaches teh object with the CharacterMovement script from the AttachPoint.
+        /// </summary>
+        /// <param name="player">Object that has the Character Movement script.</param>
+        public void DetachFromWheel(CharacterMovement player)
+        {
+            if (isDebug) SuperDebug.Log("Hamster detached!");
+            
+            var playerTransform = player.transform;
+            
+            playerTransform.position = _lastKnownPosition;
+            playerTransform.rotation = _lastKnownRotation;
+            
+            player.GetCharacterController().enabled = true;
         }
     }
 }
