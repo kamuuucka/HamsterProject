ff01a1ab3504126ec6bc096ab1657b22c7c85f4f
Kamila <kamila.matuszak@list.pl>
2025-11-04 16:26:18 +0100

    Added death colliders to the level

--- HamsterDevelopment/Assets/Scripts/Prototypes/Character/CharacterDeath.cs
+++ HamsterDevelopment/Assets/Scripts/Prototypes/Character/CharacterDeath.cs
@@ -10,27 +10,27 @@ public class CharacterDeath : MonoBehaviour
     [SerializeField] private UnityEvent<Transform> onCharacterDeath;
     [Tooltip("Layers that will cause character's death.")]
     [SerializeField] private LayerMask deathLayers;
-    [Tooltip("Height at which character will die.")][Range(0,-10)]
-    [SerializeField] private float deadlyHeight = -5f;
-    [Tooltip("Set if the character should die by height.")]
-    [SerializeField] private bool dieByHeight = true;
-    [Space(10)]
+    // [Tooltip("Height at which character will die.")][Range(0,-10)]
+    // [SerializeField] private float deadlyHeight = -5f;
+    // [Tooltip("Set if the character should die by height.")]
+    // [SerializeField] private bool dieByHeight = true;
+    // [Space(10)]
     [SerializeField] private bool isDebug;
-    [Tooltip("Show the gizmo for the deadly height value.")]
-    [SerializeField] private bool showDeadlyHeight;
-    private CharacterMovement _cm;
+    // [Tooltip("Show the gizmo for the deadly height value.")]
+    // [SerializeField] private bool showDeadlyHeight;
+    // private CharacterMovement _cm;
 
     private void Start()
     {
-        _cm = GetComponent<CharacterMovement>();
+        //_cm = GetComponent<CharacterMovement>();
     }
 
     private void Update()
     {
-        if (dieByHeight && !_cm.Grounded && transform.position.y <= deadlyHeight)
-        {
-            onCharacterDeath?.Invoke(transform);
-        }
+        // if (dieByHeight && !_cm.Grounded && transform.position.y <= deadlyHeight)
+        // {
+        //     onCharacterDeath?.Invoke(transform);
+        // }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -42,13 +42,13 @@ public class CharacterDeath : MonoBehaviour
         }
     }
     
-    private void OnDrawGizmosSelected()
-    {
-        if (showDeadlyHeight)
-        {
-            Gizmos.color = Color.red;
-            var position = transform.position;
-            Gizmos.DrawSphere(new Vector3(position.x, deadlyHeight, position.z), 0.2f);
-        }
-    }
+    // private void OnDrawGizmosSelected()
+    // {
+    //     if (showDeadlyHeight)
+    //     {
+    //         Gizmos.color = Color.red;
+    //         var position = transform.position;
+    //         Gizmos.DrawSphere(new Vector3(position.x, deadlyHeight, position.z), 0.2f);
+    //     }
+    // }
 }
