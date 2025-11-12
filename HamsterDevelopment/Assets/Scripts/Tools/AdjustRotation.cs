1f1e0c30a7d90f6f1d9d919d9fc0342e9364518d
Kamila <kamila.matuszak@list.pl>
2025-11-12 09:56:36 +0100

    Rotation works!
    - Added the methods to set both global and local rotation
    - Added documentation
    - Added the script to the light switch

--- HamsterDevelopment/Assets/Scripts/Tools/AdjustRotation.cs
+++ HamsterDevelopment/Assets/Scripts/Tools/AdjustRotation.cs
@@ -1,11 +1,28 @@
 using UnityEngine;
 
+/// <summary>
+/// Sets a new rotation for the object.
+/// </summary>
 public class AdjustRotation : MonoBehaviour
 {
+    [Tooltip("New rotation for the object.")]
     [SerializeField] private Quaternion newRotation;
 
+    /// <summary>
+    /// Sets the new local rotation. (Does take the parent rotation into the consideration.)
+    /// </summary>
+    public void SetNewLocalRotation()
+    {
+        transform.localRotation = newRotation;
+    }
+
+    /// <summary>
+    /// Sets the new global rotation. (Does NOT take the parent rotation into the consideration.)
+    /// </summary>
     public void SetNewRotation()
     {
         transform.rotation = newRotation;
     }
+    
+    
 }
