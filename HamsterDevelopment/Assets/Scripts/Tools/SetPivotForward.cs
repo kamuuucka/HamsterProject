46db45bbdf3589bc70134ccb47a830cffbf51e30
Noah <noahpbrinkman@gmail.com>
2026-05-01 14:33:15 +0200

    New slingshot

--- HamsterDevelopment/Assets/Scripts/Tools/SetPivotForward.cs
+++ HamsterDevelopment/Assets/Scripts/Tools/SetPivotForward.cs
@@ -8,25 +8,48 @@ public class SetPivotForward : MonoBehaviour
     [SerializeField] private CharacterPivot pivot;
     [SerializeField] private bool useSelfTransform = true;
     [SerializeField, ConditionalField(nameof(useSelfTransform), true)] private Vector3 forward;
-
-    public bool SetPivot = true;
+    [SerializeField] private bool delayPivot = false;
+    [SerializeField,ConditionalField(nameof(delayPivot))] private float delayTime = 1;
+    private bool delayed = false;
+    private float timer = 0;
+    public bool SetPivot = false;
     
     [SerializeField] private bool activateOnEnable = true;
-
+    
     private void OnEnable()
     {
         if (activateOnEnable)
         {
-            pivot.EnableCameraMovement =false;
+            
+            if (delayPivot)
+            {
+                delayed = true;
+            }
+            else
+            {
+                Activate();
+            }
         }
     }
     
     public void Activate()
     {
+        pivot.EnableCameraMovement = false;
+        SetPivot = true;
     }
+
     
     private void Update()
     {
+        if (delayPivot && !SetPivot)
+        {
+            timer += Time.deltaTime;
+            if (timer >= delayTime)
+            {
+                Activate();
+                timer = 0;
+            }
+        }
         if (SetPivot)
         {
             pivot.PivotTransform.forward = useSelfTransform ? pivot.PivotTransform.position : forward;
